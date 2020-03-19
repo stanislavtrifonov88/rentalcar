69c98beb4986fd23dde9c44e09471afe35a175cc
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-03-19 21:09:42 +0200

    transformerToContractDTO method updated

--- server/src/contracts/transformers/transformToContractDTO.ts
+++ server/src/contracts/transformers/transformToContractDTO.ts
@@ -3,23 +3,24 @@ import { Contract } from "../../database/entities/contract.entity";
 
 
 export const transformToContractDTO = async (contract: Contract) => {
-    const contractInfo = (({ 
-        id,
-        borrowerFirstName,
-        borrowerLastName,
-        borrowerAge,
-        startDate,
-        contractEndDate }) => ({         
-        id,
-        borrowerFirstName,
-        borrowerLastName,
-        borrowerAge,
-        startDate,
-        contractEndDate }))(contract);
-    const carInfo = (({ brand, model }) => ({ brand, model }))(await contract.car);
-    const a = await (await contract.car).className
-    const price = a.price
-    const individualContractFormated: IndividualContractDTO = { ...contractInfo, ...carInfo, price };
+            const contractInfo = (({ 
+                id,
+                borrowerFirstName,
+                borrowerLastName,
+                borrowerAge,
+                startDate,
+                contractEndDate }) => ({         
+                id,
+                borrowerFirstName,
+                borrowerLastName,
+                borrowerAge,
+                startDate,
+                contractEndDate }))(contract);
+            const carInfo = (({ brand, model }) => ({ brand, model }))(await contract.car);
+            const tempPrice = (contract.car).className
+            const price = tempPrice.price
+
+            const individualContractFormated: IndividualContractDTO = { ...contractInfo, ...carInfo, price};
 
     return individualContractFormated
 }
