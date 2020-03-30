de8c64f0c636f0e8e508c4a3824ef41c37bdbdd8
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-03-30 15:03:39 +0300

    contracts service + tests fixed

--- server/src/contracts/contracts.service.ts
+++ server/src/contracts/contracts.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as moment from 'moment';
+import { Repository, getManager } from 'typeorm';
 import { Car } from '../database/entities/car.entity';
-import { Repository } from 'typeorm';
 import { Contract } from '../database/entities/contract.entity';
 import { NewContractDTO } from './models/newContract.dto';
 import { IndividualContractDTO } from './models/individualContract.dto';
 import { transformToContractDTO } from './transformers/transformToContractDTO';
 import { createContractErrorHandling } from '../shared/errors/createContractErrorHandling';
-import { CarRentalSystemError } from '../shared/exceptions/carRental-system.error';
 import * as errorMessages from '../shared/errors/error.messages';
-import {getManager} from "typeorm";
 import { CarsService } from '../cars/cars.service';
 import Guard from '../shared/util/Guard';
 
@@ -18,88 +16,84 @@ import Guard from '../shared/util/Guard';
 @Injectable()
 export class ContractsService {
     estimatedDaysDiscount: any;
+
     estimatedAgeDiscount: any;
+
     public constructor(
         @InjectRepository(Car) private readonly carsRepository: Repository<Car>,
         @InjectRepository(Contract) private readonly contractsRepository: Repository<Contract>,
-        private readonly carsService: CarsService
+        private readonly carsService: CarsService,
     ) { }
 
     public async getAllContracts(
-        transformatorToDTO: (n: Contract) => Promise<IndividualContractDTO> = transformToContractDTO
-        ): Promise<IndividualContractDTO[]> {
-        const allContractsData: Contract[] = await this.contractsRepository.find({
-            where: {
-                deliveredDate: null,
-                isDeleted: false,
-            },
-        });
+      transformatorToDTO: (n: Contract) => Promise<IndividualContractDTO> = transformToContractDTO,
+    ): Promise<IndividualContractDTO[]> {
+      const allContractsData: Contract[] = await this.contractsRepository.find({
+        where: {
+          deliveredDate: null,
+          isDeleted: false,
+        },
+      });
 
-        let allContractsDataFormated:  IndividualContractDTO[] = []
+      let allContractsDataFormated: IndividualContractDTO[] = [];
 
-        allContractsData.map(async (contract: Contract) => {
-            const individualContractFormated: IndividualContractDTO = await transformatorToDTO(contract)
-            allContractsDataFormated = [...allContractsDataFormated, individualContractFormated];
-        })
-        await Promise.resolve(allContractsDataFormated)
+      allContractsData.map(async (contract: Contract) => {
+        const individualContractFormated: IndividualContractDTO = await transformatorToDTO(contract);
+        allContractsDataFormated = [...allContractsDataFormated, individualContractFormated];
+      });
+      await Promise.resolve(allContractsDataFormated);
 
-        return await allContractsDataFormated;
+      return allContractsDataFormated;
     }
 
-    public async createContract(body: NewContractDTO, carId: string): Promise<IndividualContractDTO> {
-        
-        const foundCar: Car = await this.carsService.getAvailableCarById(carId)
+    public async createContract(
+        body: NewContractDTO, 
+        carId: string
+        ): Promise<IndividualContractDTO> {
+      const foundCar: Car = await this.carsService.getAvailableCarById(carId);
 
-        createContractErrorHandling(body)
+      createContractErrorHandling(body);
 
-        const createdContract: Contract = await getManager().transaction(async transactionalEntityManager => {
-            const newContract = await this.contractsRepository.create(body)
-            newContract.car = foundCar
-            const createdContract = await transactionalEntityManager.save(newContract);
-            foundCar.isBorrowed = true
-            await transactionalEntityManager.save(foundCar);
+      const createdContract: Contract = await getManager().transaction(async (transactionalEntityManager) => {
+        const newContract = this.contractsRepository.create(body);
+        newContract.car = foundCar;
+        const createdContract = await transactionalEntityManager.save(newContract);
+        foundCar.isBorrowed = true;
+        await transactionalEntityManager.save(foundCar);
 
-            return createdContract
-        });
+        return createdContract;
+      });
 
-        const individualContractFormated: IndividualContractDTO = await transformToContractDTO(createdContract)
+      const individualContractFormated: IndividualContractDTO = await transformToContractDTO(createdContract);
 
-        return individualContractFormated;
+      return individualContractFormated;
     }
 
-    public async returnCar(contractId: string, body: {name: number}): Promise<IndividualContractDTO> {
+    public async returnCar(
+        contractId: string, body: {name: number},
+        ): Promise<IndividualContractDTO> {
+      const foundContract = await this.contractsRepository.findOne({
+        where: {
+          id: contractId,
+          deliveredDate: null,
+        },
+      });
 
-        let foundContract = await this.contractsRepository.findOne({
-            where: {
-                id: contractId,
-                deliveredDate: null,
-            },
-        });
+      Guard.isFound(foundContract, errorMessages.contractNotFound);
+      const foundCar = await this.carsService.getBorrowedCarById(foundContract.car.id)
 
-        Guard.isFound(foundContract,errorMessages.contractNotFound.msg);
+      await getManager().transaction(async (transactionalEntityManager) => {
+        foundCar.isBorrowed = false;
+        await this.carsRepository.save(foundCar);
 
-        let foundCar = await this.carsRepository.findOne({
-            where: {
-                id: (await foundContract.car).id,
-                isBorrowed: true,
-                isDeleted: false,
-            },
-        });
-        Guard.isFound(foundContract,errorMessages.borrowedCarNotFound.msg);
+        foundContract.deliveredDate = moment(new Date()).format('YYYY-MM-DDTHH:mm');
+        foundContract.pricePaid = body.name;
 
-        await getManager().transaction(async transactionalEntityManager => {
-            foundCar.isBorrowed = false;
-            await this.carsRepository.save(foundCar)
-    
-            foundContract.deliveredDate = moment(new Date()).format('YYYY-MM-DDTHH:mm')
-            foundContract.pricePaid = body.name;
+        await this.contractsRepository.save(foundContract);
+      });
 
-            await this.contractsRepository.save(foundContract)
-        });
+      const individualContractFormated: IndividualContractDTO = await transformToContractDTO(foundContract);
 
-        const individualContractFormated: IndividualContractDTO = await transformToContractDTO(foundContract)
-
-        return individualContractFormated;
+      return individualContractFormated;
     }
-
 }
