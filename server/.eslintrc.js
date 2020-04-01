1e607780634786bb03dc14bf84cdbef2dfe7013c
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-01 11:28:06 +0300

    eslinttrc.js removed comments

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -11,10 +11,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'prettier/@typescript-eslint',
-    // 'plugin:@typescript-eslint/eslint-recommended',
-    // 'plugin:@typescript-eslint/recommended',
-    // 'prettier',
-    // 'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
