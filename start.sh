c5223a8e77942387bf78dd70cec9336e47f404e5
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-03 17:37:58 +0300

    dash script adjusted for no seed file

--- start.sh
+++ start.sh
@@ -58,19 +58,13 @@ sed -i "s/YOUR_PASSWORD/$password/g" ormconfig.json
 sed -i "s/YOUR_DATABASE/$database/g" ormconfig.json
 
 # docker run --name $database -p 5432:5432 -e POSTGRES_PASSWORD=$password -e POSTGRES_USER=$username -d postgres:11.5
-# Need to uncomment the previous line and also make sure that i can always take the latest id
 # Why are permission required to start the file for a first time??? 1) pwd 2)chmod a+x /home/strifonov/React_Play/BASH/test.sh  
-
 
 docker container start $database
 
 # npm i
 echo "Running the migrations ..."
 npm run typeorm:run
-echo "Running the seed file ..."
-npm run seed
-
-# every time you run the bash script the seed executes. How to avoid that?
 
 cd ../
 echo "Setting up the client ..."
