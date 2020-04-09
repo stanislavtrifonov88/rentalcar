4f216083bb07b7a9c67190795723b12253fc548a
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-09 10:03:32 +0300

    spinner  + css flexbox in dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -4,31 +4,37 @@ import './Dashboard.css';
 import DashboardItem from './DashboardItem/DashboardItem';
 import fetchRequest from '../../services/Rest';
 import { baseURL, contracts }from '../../services/restAPIs/restAPIs'
+import Spinner from '../Spinner/Spinner';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       contracts: [],
+      loading: false,
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     fetchRequest(`${baseURL}/${contracts}`)
       .then((result) => {
         this.setState({
           contracts: result,
+          loading: false,
         });
       });
   }
 
   onSubmit = (name, id) => {
+    this.setState({ loading: true });
       fetchRequest(`${baseURL}/${contracts}/${id}`, 'PUT', {name})
       .then(response => 
         fetchRequest(`${baseURL}/${contracts}`)
         .then((result) => {
           this.setState({
             contracts: result,
+            loading: false,
           });
         }));
   }
@@ -36,31 +42,37 @@ class Dashboard extends React.Component {
 
   render() {
     const contracts = this.state.contracts.map((contract) => <DashboardItem key={contract.id} contract={contract} onChildClick={this.onSubmit} />);
+    const { loading } = this.state;
+    let table = <table className="dashboarTable">
+    <thead>
+      <tr className="headerRow">
+        <th className="rowLength1">Car</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th className="rowLength1">From</th>
+        <th className="rowLength1">Return Date</th>
+        <th>Days Rented</th>
+        <th>Price | Day</th>
+        <th>Current Days Rented</th>
+        <th>Days Overdue</th>
+        <th>Current Price | Day</th>
+        <th>Current Total Price</th>
+        <th>Return Car</th>
+      </tr>
+    </thead>
+    <tbody className="dataRows">
+      {contracts}
+    </tbody>
+  </table>
+   
+    if (loading) {
+      table = <Spinner className="spinnerDashboard" />
+    }
 
     return (
       <div className="dashboardContainer">
         <h1>Rented Cars</h1>
-        <table className="dashboarTable">
-          <thead>
-            <tr className="headerRow">
-              <th className="rowLength1">Car</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th className="rowLength1">From</th>
-              <th className="rowLength1">Return Date</th>
-              <th>Days Rented</th>
-              <th>Price | Day</th>
-              <th>Current Days Rented</th>
-              <th>Days Overdue</th>
-              <th>Current Price | Day</th>
-              <th>Current Total Price</th>
-              <th>Return Car</th>
-            </tr>
-          </thead>
-          <tbody className="dataRows">
-            {contracts}
-          </tbody>
-        </table>
+        {table}
       </div>
     );
   }
