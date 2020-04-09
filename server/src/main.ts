01c3f75590a027defbcc412b8ca6c43d9329dcf9
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-09 14:56:19 +0300

    useStaticAssets implemented

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,20 @@
 import "reflect-metadata";
 
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { CarRentalSystemErrorFilter } from "./shared/filters/carRental-error.filter";
 
-
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
 
   app.useGlobalFilters(new CarRentalSystemErrorFilter());
+  app.useStaticAssets(join(__dirname, '..', 'assets'), {prefix: '/img'});
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
