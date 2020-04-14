ab208ea8017e35c668a5ccdfc9b7ec7392723b4e
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-14 18:42:59 +0300

    should() reworked with consistent tests

--- server/src/shared/errors/createContractErrorHandling.ts
+++ server/src/shared/errors/createContractErrorHandling.ts
@@ -1,26 +1,27 @@
 import * as moment from 'moment';
 import { NewContractDTO } from '../../contracts/models/newContract.dto';
 import * as errorMessages from './error.messages';
-import { should }from '../util/Guard'
+import { should } from '../util/Guard';
+import { dateFormat } from '../constants/constants';
 
 export const createContractErrorHandling = (body: NewContractDTO): void => {
-  const now = (moment(new Date(), 'YYYY-MM-DDTHH:mm').add(-15, 'minutes')).format('YYYY-MM-DDTHH:mm');
+  const now = (moment(new Date(), dateFormat).add(-15, 'minutes')).format(dateFormat);
 
-  should((moment(body.startDate).isValid()), errorMessages.validStartDate)
+  should(moment(body.startDate).isValid(), errorMessages.validStartDate)
 
-  should((moment(body.contractEndDate).isValid()), errorMessages.validEndDate)
+  should(moment(body.contractEndDate).isValid(), errorMessages.validEndDate)
 
-  should(!(now > body.startDate), errorMessages.startDateInThePast)
+  should(now < body.startDate, errorMessages.startDateInThePast)
 
-  should(!(body.startDate > body.contractEndDate), errorMessages.returnDateInThePast)
+  should(body.startDate < body.contractEndDate, errorMessages.returnDateInThePast)
 
-  should(!(body.borrowerAge < 18), errorMessages.ageBelow18)
+  should(body.borrowerAge >= 18, errorMessages.ageBelow18)
 
-  should(!(body.borrowerFirstName.length < 2), errorMessages.firstNameMinLength)
+  should(body.borrowerFirstName.length >= 2, errorMessages.firstNameMinLength)
 
-  should(!(body.borrowerFirstName.length > 25), errorMessages.firstNamemaxLength)
+  should(body.borrowerFirstName.length < 25, errorMessages.firstNamemaxLength)
 
-  should(!(body.borrowerLastName.length < 2), errorMessages.lastNameMinLength)
+  should(body.borrowerLastName.length >= 2, errorMessages.lastNameMinLength)
 
-  should(!(body.borrowerLastName.length > 25), errorMessages.lastNamemaxLength)
+  should(body.borrowerLastName.length < 25, errorMessages.lastNamemaxLength)
 };
