cb856abade2a1ea2ec03c6c40f01617b7f122376
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-16 10:53:46 +0300

    dateModifers expanded

--- server/src/shared/constants/constants.ts
+++ server/src/shared/constants/constants.ts
@@ -4,6 +4,8 @@ export const dateFormat = 'YYYY-MM-DDTHH:mm';
 export const timeStamp = ( days = 0, minutes = 0) => {
     return moment(new Date()).add(days, 'days').add(minutes, 'minutes').format(dateFormat);
 }
-
+export const differenceInDays = (endTime, startTime) => moment
+  .duration(moment(endTime, dateFormat)
+    .diff(moment(startTime, dateFormat))).asDays();
 
 
