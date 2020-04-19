40dfb4f56cbce54498f60388db4d25c155a5a6c7
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-19 11:20:30 +0300

    search field: draft 1 - working

---
+++ client/src/components/SearchBar/SearchInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class SearchInput extends React.Component {
+  constructor() {
+    super();
+  }
+
+  onValueChange = (event) => {
+
+    this.props.update(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="search-input">
+        <input
+          id="input"
+          onChange={this.onValueChange}
+          value={this.props.value}
+          type="text"
+          placeholder="Search here..."
+        />
+      </div>
+    );
+  }
+}
