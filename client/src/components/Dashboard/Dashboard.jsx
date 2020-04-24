77406e56ee08c3ed7c28f647f5414d780eb5e559
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-24 11:00:49 +0300

    dashboard new search and filters logic

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ import { baseURL, contracts }from '../../services/restAPIs/restAPIs'
 import Spinner from '../Spinner/Spinner';
 import { toastSuccess } from '../../services/toastify/toastify';
 import SearchInput from '../SearchBar/SearchInput';
-import {createList, applyFilters } from '../Filters/filterFunctions';
+import {createList, applyFilters, applySearch } from '../Filters/filterFunctions';
 import Select from '../Filters/Select';
 
 class Dashboard extends React.Component {
@@ -17,10 +17,13 @@ class Dashboard extends React.Component {
       loading: false,
       filteredList: [],
       searchString: '',
+      filterStrings: {
+        brand: '',
+        model: '',
+      },
       filters: {
         brand: { isOpen: false },
         model: { isOpen: false },
-        class: { isOpen: false },
       }
     };
   }
@@ -50,51 +53,56 @@ class Dashboard extends React.Component {
         }));
   }
 
-  filterList = (value) => {
-    const { contracts } = this.state
-    let searchValue = value.trim().toLowerCase();
-    let filteredList = contracts;
-    filteredList = filteredList.filter(item => {
-      if (item.brand.toLowerCase().search(searchValue) !== -1) {
-        return item.brand.toLowerCase().search(searchValue) !== -1;
-      } else if (item.model.toLowerCase().search(searchValue) !== -1) {
-        return item.model.toLowerCase().search(searchValue) !== -1;
-      } else if (item.borrowerFirstName.toLowerCase().search(searchValue) !== -1) {
-        return item.borrowerFirstName.toLowerCase().search(searchValue) !== -1;
-      } else if (item.borrowerLastName.toLowerCase().search(searchValue) !== -1) {
-        return item.borrowerLastName.toLowerCase().search(searchValue) !== -1;
-      }
-    });
+//   filterList = (value) => {
+//     const { contracts } = this.state
+//     let searchValue = value.trim().toLowerCase();
+//     let filteredList = contracts;
+//     filteredList = filteredList.filter(item => {
+//       if (item.brand.toLowerCase().search(searchValue) !== -1) {
+//         return item.brand.toLowerCase().search(searchValue) !== -1;
+//       } else if (item.model.toLowerCase().search(searchValue) !== -1) {
+//         return item.model.toLowerCase().search(searchValue) !== -1;
+//       } else if (item.borrowerFirstName.toLowerCase().search(searchValue) !== -1) {
+//         return item.borrowerFirstName.toLowerCase().search(searchValue) !== -1;
+//       } else if (item.borrowerLastName.toLowerCase().search(searchValue) !== -1) {
+//         return item.borrowerLastName.toLowerCase().search(searchValue) !== -1;
+//       }
+//     });
 
-    this.setState({filteredList: filteredList});
-    this.setState({searchString: value});
+//     this.setState({filteredList: filteredList});
+//     this.setState({searchString: value});
+// }
+
+searchString = (value) => {
+  this.setState({searchString: value});
 }
 
-// filterByBrand = (criteria) => {
+filterBy = (data) => {
+let { filterStrings } = this.state;
+filterStrings[data.dataAttribute] = data.option.value
+if (data.option.value === 'None') {
+  filterStrings[data.dataAttribute] = ''
+}
 
-//   let { filteredList } = this.state;
-//   const { contracts } = this.state
-//   filteredList = applyFilters(filteredList, contracts, criteria, 'brand')
+this.setState({ filterStrings });
+}
 
-//   this.setState({filteredList});
-// }
-
-// filterByModel = (criteria) => {
-//   let { filteredList } = this.state;
-//   const { contracts } = this.state
-//   filteredList = applyFilters(filteredList, contracts, criteria, 'model')
-
-//   this.setState({filteredList});
-// }
 
   render() {
-    let { filteredList } = this.state
-    const { contracts } = this.state
+    // let { filteredList } = this.state
+    // const { contracts } = this.state
+    let { contracts, filterStrings, searchString } = this.state;
+    contracts = applyFilters(contracts, filterStrings)
+    console.log(contracts)
+    contracts = applySearch(contracts, searchString, ['brand', 'model', 'borrowerFirstName', 'borrowerLastName'])
+    console.log(contracts)
+    const brandsList = createList(contracts, 'id', 'brand')
+    const modelsList = createList(contracts, 'id', 'model')
 
-    if (filteredList.length === 0) {
-      filteredList = contracts;
-    }
-    const contracts1 = filteredList.map((contract) => <DashboardItem key={contract.id} contract={contract} onChildClick={this.onSubmit} />);
+    // if (filteredList.length === 0) {
+    //   filteredList = contracts;
+    // }
+    const filteredContracts = contracts.map((contract) => <DashboardItem key={contract.id} contract={contract} onChildClick={this.onSubmit} />);
     const { loading } = this.state;
     let table = <table className="dashboarTable">
     <thead>
@@ -115,7 +123,7 @@ class Dashboard extends React.Component {
       </tr>
     </thead>
     <tbody className="dataRows">
-      {contracts1}
+      {filteredContracts}
     </tbody>
   </table>
    
@@ -123,18 +131,14 @@ class Dashboard extends React.Component {
       table = <Spinner className="spinnerDashboard" />
     }
 
-    const brandsList = createList(filteredList, 'id', 'brand')
-    const modelsList = createList(filteredList, 'id', 'model')
-
-
     return (
       <div className="dashboardContainer" data-element="dashboard">
         <h1>Rented Cars</h1>
         <div className="filterContainer">
-        <SearchInput value={this.state.searchString} update={this.filterList} /> 
+        <SearchInput value={this.state.searchString} update={this.searchString} /> 
         <div className="availableDashboardFiltersContainer">
-          <Select options={brandsList} onChildClick={this.filterByBrand} type={'Brand'}/> 
-          <Select options={modelsList} onChildClick={this.filterByModel} type={'Model'} />
+          <Select options={brandsList} onChildClick={this.filterBy} type={'Brand'} dataFilter={"brand"}/> 
+          <Select options={modelsList} onChildClick={this.filterBy} type={'Model'} dataFilter={"model"}/>
         </div>
       </div>
         {table}
