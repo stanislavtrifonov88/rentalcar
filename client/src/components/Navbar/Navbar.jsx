de17e1bd2788d8b2fac27ceb74ce75988d45f24a
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-27 09:31:55 +0300

    removed old searchbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -23,15 +23,13 @@ class NavigationBarCars extends React.Component {
     return (
       <div className="Navbar" data-element="navbar">
         <div className="leftSideContainer">
-        <p className="brandName" href="/">Awesome Cars</p>
+        <a className="brandName" href="http://localhost:8000/">Awesome Cars</a>
         <div className="NavbarBtnContainer">
         <Link className="StandardLink" onClick={() => this.props.onSearch('')} to="/" data-element="navbarHomeLink">Home</Link>
         <Link className="StandardLink" to="/dashboard" data-element="navbarDashboardLink">Dashboard</Link>
         </div>
         </div>
         <div className="rightSideContainer">
-       <input className="SearchField" type="text"  placeholder="Search here" data-name="searchWord"  onChange={this.onInputChange}></input>
-       <Link className="SearchBtn" onClick={() => this.props.onSearch(this.state.searchWord)} to="/" data-element="navbarSearchBtn">Search</Link>
         </div>
       </div>
     );
