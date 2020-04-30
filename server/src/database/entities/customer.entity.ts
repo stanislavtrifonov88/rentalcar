dce1c0e74546a11b9f048582eae59d3141133ff8
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-04-30 14:37:31 +0300

    customer entity created

---
+++ server/src/database/entities/customer.entity.ts
@@ -0,0 +1,35 @@
+import {
+    Entity,
+    Column,
+    OneToMany,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+} from 'typeorm';
+import { Contract } from './contract.entity'
+import { Car } from './car.entity';
+
+
+@Entity('customers')
+export class Customer {
+    // @PrimaryGeneratedColumn('uuid')
+    // id: string;
+    @PrimaryColumn({ type: 'bigint', nullable: false })
+    phone: number;
+
+    @Column({ type: 'text', nullable: false })
+    firstName: string;
+
+    @Column({ type: 'text', nullable: false })
+    lastName: string;
+
+    @Column({ type: 'date', nullable:false })
+    birthdate: Date;
+
+    @Column({ type: 'boolean', default: false })
+    isDeleted: boolean;
+
+    @OneToMany(type => Contract, contract => contract.customer)
+    contracts: Promise<Contract[]>;
+
+}
