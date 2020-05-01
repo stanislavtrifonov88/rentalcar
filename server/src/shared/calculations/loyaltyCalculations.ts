446ec462ffea28254d01faa400596aff85db1001
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-01 11:58:09 +0300

    loyaltyDiscounts constants added

--- server/src/shared/calculations/loyaltyCalculations.ts
+++ server/src/shared/calculations/loyaltyCalculations.ts
@@ -1,19 +1,19 @@
-
+import * as loyaltyDiscounts from '../discounts/loyaltyDiscounts';
 
 export const loyaltyDiscount = (customerData) => {
     const previousContracts = customerData.contracts.length;
    let discount = 0
 
     if (previousContracts === 0) {
-        discount = 0.2;
+        discount = loyaltyDiscounts.previousContracts0;
     } else if(previousContracts >= 1 && previousContracts <= 4) {
-        discount = 0;
+        discount = loyaltyDiscounts.previousContracts1To4;
     }  else if(previousContracts >= 5 && previousContracts <= 9) {
-        discount = 0.5;
+        discount = loyaltyDiscounts.previousContracts5To9;
     }  else if(previousContracts >= 10 && previousContracts <= 19) {
-        discount = 0.7;
+        discount = loyaltyDiscounts.previousContracts10To19;
     } else if (previousContracts >= 20) {
-        discount = 0.7;
+        discount = loyaltyDiscounts.previousContractsAbove20;
     }
 
   return discount;
