481fab26c9d27385cbcee42934a4f5ffa65bf59e
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-01 11:27:38 +0300

    age added to contracts DTOs

--- server/src/contracts/transformers/transformToContractDTO.ts
+++ server/src/contracts/transformers/transformToContractDTO.ts
@@ -1,5 +1,6 @@
 import { IndividualContractDTO } from "../models/individualContract.dto";
 import { Contract } from "../../database/entities/contract.entity";
+import { differenceInYears } from "../../shared/constants/dateModifiers";
 
 
 export const transformToContractDTO = async (contract: Contract): Promise<IndividualContractDTO> => {
@@ -12,10 +13,11 @@ export const transformToContractDTO = async (contract: Contract): Promise<Indivi
                 contractEndDate }))(contract);
             const carInfo = (({ brand, model }): any => ({ brand, model }))(contract.car);
             const customerInfo = (({ firstName, lastName, birthdate}): any => ({ firstName, lastName, birthdate }))(contract.customer);
-            const tempPrice = (contract.car).className
-            const {price} = tempPrice
+            const tempPrice = (contract.car).className;
+            const {price} = tempPrice;
+            const age = differenceInYears(customerInfo.birthdate)
 
-            const individualContractFormated: IndividualContractDTO = { ...contractInfo, ...carInfo, ...customerInfo, price};
+            const individualContractFormated: IndividualContractDTO = { ...contractInfo, ...carInfo, ...customerInfo, age, price};
 
     return individualContractFormated
 }
