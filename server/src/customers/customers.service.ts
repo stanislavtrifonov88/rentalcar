bd39f5dcc710cd3c4c4ac220c2f0097630c749b3
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-02 10:33:46 +0300

    error msgs for customers service added

--- server/src/customers/customers.service.ts
+++ server/src/customers/customers.service.ts
@@ -5,6 +5,8 @@ import { transformToCustomerDTO } from './transformers/transformToCustomerDTO';
 import { IndividualCustomerDTO } from './models/individualCustomerDTO';
 import { NewCustomerDTO } from './models/newCustomerDTO';
 import { Repository, getManager } from 'typeorm';
+import * as Guard from '../shared/util/Guard';
+import * as errorMessages from '../shared/errors/error.messages';
 
 @Injectable()
 export class CustomersService {
@@ -17,13 +19,14 @@ export class CustomersService {
         const foundCustomer: Customer = await this.customersRepository.findOne({
         where: {
             phone,
-            isDeleted: false,
         },
         relations: [ 'contracts' ]
     })
+
+    Guard.isFound(foundCustomer, errorMessages.customerNotFound);
+    Guard.isFound(!foundCustomer.isDeleted, errorMessages.customerDeleted);
+
     const transformedCustomer: IndividualCustomerDTO = await transformToCustomerDTO(foundCustomer)
-    // Guard.isFound(foundCar, errorMessages.carNotFound);
-    // Guard.isFound(!foundCar.isBorrowed, errorMessages.carIsBorrowed);
 
     return transformedCustomer;
 }
@@ -37,14 +40,10 @@ public async createNewCustomer(body: NewCustomerDTO): Promise<IndividualCustomer
     },
     relations: [ 'contracts' ]
 })
-console.log(foundCustomer)
 
-if (foundCustomer) {
-  return
-}
-//   createContractErrorHandling(body);
+Guard.isFound(!foundCustomer, errorMessages.customerAlreadyExist);
+
   const newCustomer: Customer = await this.customersRepository.create(body);
-  // await this.customersRepository.save(newCustomer)
 
   await getManager().transaction(async (transactionalEntityManager) => {
     await transactionalEntityManager.save(newCustomer);
