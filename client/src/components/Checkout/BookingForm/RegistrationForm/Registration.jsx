4ed18c51d9e90f61c4ab955150ef9c06b15dd819
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-05 15:52:16 +0300

    Registration component added

---
+++ client/src/components/Checkout/BookingForm/RegistrationForm/Registration.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { timeStamp } from '../../../../shared/dateModifiers';
+import { isValidField } from '../../Validations/validationChecks';
+
+const Registration = ({ newCustomerHandler, validations, onCancel, onRegistrationSubmit }) => {
+  const validationErrorFirstName = isValidField(validations.firstName);
+  const validationErrorLastName = isValidField(validations.lastName);
+  const validationErrorBirthdate = isValidField(validations.birthdate);
+  const validationErrorDate = isValidField(validations.contractEndDate);
+
+  return (
+    <>
+      <div className="inputRows">
+        <div className="inputFormContainer">
+          <label htmlFor="FirstName">
+            First name
+          </label>
+          <input
+            id="FirstName"
+            className="firstNameField"
+            required
+            type="text"
+            placeholder="First name"
+            data-name="firstName"
+            onChange={newCustomerHandler}
+          />
+
+          <p className={validationErrorFirstName}>First name is invalid.</p>
+        </div>
+        <div className="inputFormContainer">
+          <label htmlFor="LastName">
+            Last className:
+          </label>
+          <input
+            id="LastName"
+            className="lastNameField"
+            required
+            type="text"
+            placeholder="Last name"
+            data-name="lastName"
+            onChange={newCustomerHandler}
+          />
+
+          <p className={validationErrorLastName}>Last name is invalid.</p>
+        </div>
+      </div>
+      <div className="inputRows">
+        <div className="inputFormContainer">
+          <label htmlFor="birthdate">
+            Birthdate
+          </label>
+          <input
+            id="birthdate"
+            className="birthdateField"
+            type="date"
+            placeholder="Birthdate"
+            required
+            data-name="birthdate"
+            onChange={newCustomerHandler}
+          />
+
+          <p className={validationErrorBirthdate}>Please enter a valid birthdate.</p>
+        </div>
+      </div>
+      <div className="checkoutBtnsContainer">
+        <button type="submit" className="bookCarBtn" onClick={onRegistrationSubmit} data-element="bookingFormCheckoutBtn">
+          Checkout
+        </button>
+        <button type="submit" className="bookCarBtn" onClick={onCancel}>Cancel</button>
+      </div>
+    </>
+  );
+};
+export default Registration;
