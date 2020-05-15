0b7acd75880dc3a90d0adc2ddac30720a24f298b
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-15 09:05:37 +0300

    loyalty discounts fixed

--- server/src/shared/discounts/loyaltyDiscounts.ts
+++ server/src/shared/discounts/loyaltyDiscounts.ts
@@ -1,5 +1,5 @@
-export const previousContracts0 = 0.2;
+export const previousContracts0 = -0.2;
 export const previousContracts1To4 = 0;
-export const previousContracts5To9 = 0.05;
-export const previousContracts10To19 = 0.07;
-export const previousContractsAbove20 = 0.07;
+export const previousContracts5To9 = -0.05;
+export const previousContracts10To19 = -0.07;
+export const previousContractsAbove20 = -0.1;
