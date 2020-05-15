e428077e3ed8d884c1615c5dc59ed8c570f3258f
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-15 09:57:20 +0300

    transformCustomerDTO and customerService working

--- server/src/customers/transformers/transformToCustomerDTO.ts
+++ server/src/customers/transformers/transformToCustomerDTO.ts
@@ -6,8 +6,11 @@ import { differenceInYears } from "../../shared/constants/dateModifiers";
 
 export const transformToCustomerDTO = async (customer: Customer): Promise<IndividualCustomerDTO> => {
     const customerPropsPicked = (({ phone, firstName, lastName, birthdate }): any => ({ phone, firstName, lastName, birthdate }))(customer);
-    const loyaltyDiscountPercent = loyaltyDiscount(customer);
-    const geoDiscountPercent = geoDiscount(customer);
+    const geoDiscountPercent = geoDiscount(customerPropsPicked);
+    console.log(customerPropsPicked)
+    const contracts = Promise.resolve(customer.contracts)
+    const loyaltyDiscountPercent = await loyaltyDiscount((await contracts).length);
+    console.log(geoDiscount)
     const age = differenceInYears(customer.birthdate)
 
     const finalCustomerObject = {...customerPropsPicked, age ,loyaltyDiscount: loyaltyDiscountPercent, geoDiscount: geoDiscountPercent}
