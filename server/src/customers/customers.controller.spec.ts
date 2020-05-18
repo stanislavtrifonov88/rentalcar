9bfcf9828acecd73473f64ddfcd12e064e9a540d
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-18 17:39:50 +0300

    customers testing set-up

--- server/src/customers/customers.controller.spec.ts
+++ server/src/customers/customers.controller.spec.ts
@@ -1,18 +1,32 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { CustomersController } from './customers.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
 
-// describe('Customers Controller', () => {
-//   let controller: CustomersController;
+describe('Customers Controller', () => {
+  let controller: CustomersController;
+  const customersService = {
+    getCustomerByPhone(): any {
+      /* empty */
+    },
+    createNewCustomer(): any {
+      /* empty */
+    },
+    findCustomerByPhone(): any {
+        /* empty */
+      },
+  };
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [CustomersController],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CustomersController],
+      providers: [{ provide: CustomersService, useValue: customersService }],
+    }).compile();
 
-//     controller = module.get<CustomersController>(CustomersController);
-//   });
+    controller = module.get<CustomersController>(CustomersController);
+  });
 
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+});
--- server/src/customers/customers.service.spec.ts
+++ server/src/customers/customers.service.spec.ts
@@ -1,18 +1,44 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { CustomersService } from './customers.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CustomersService } from './customers.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Customer } from '../database/entities/customer.entity';
 
-// describe('CustomersService', () => {
-//   let service: CustomersService;
-
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [CustomersService],
-//     }).compile();
-
-//     service = module.get<CustomersService>(CustomersService);
-//   });
-
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+describe('CustomersService', () => {
+    let service: CustomersService;
+  
+    const customerRepository = {
+      find(): any {
+        /* empty */
+      },
+      findOne(): any {
+        /* empty */
+      },
+      save(): any {
+        /* empty */
+      },
+      create(): any {
+        /* empty */
+      },
+    };
+  
+  
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          CustomersService,
+          {
+            provide: getRepositoryToken(Customer),
+            useValue: customerRepository,
+          },
+        ],
+      }).compile();
+  
+      service = module.get<CustomersService>(CustomersService);
+  
+    });
+  
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+  
+  });
