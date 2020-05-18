9e9dcfe0f08e9b61f1499da392ac2ad435869cb5
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-18 14:36:44 +0300

    transformToCustomerDTO updated

--- server/src/customers/transformers/transformToCustomerDTO.ts
+++ server/src/customers/transformers/transformToCustomerDTO.ts
@@ -1,14 +1,11 @@
 import { Customer } from "../../database/entities/customer.entity";
 import { IndividualCustomerDTO } from "../models/individualCustomerDTO";
-import { loyaltyDiscount, geoDiscount } from "../../shared/calculations/loyaltyCalculations";
 import { differenceInYears } from "../../shared/constants/dateModifiers";
 
 
 export const transformToCustomerDTO = async (customer: Customer): Promise<IndividualCustomerDTO> => {
     const customerPropsPicked = (({ phone, firstName, lastName, birthdate }): any => ({ phone, firstName, lastName, birthdate }))(customer);
-    // const geoDiscountPercent = geoDiscount(customerPropsPicked);
     const contracts = Promise.resolve(customer.contracts)
-    // const loyaltyDiscountPercent = await loyaltyDiscount((await contracts).length);
     const previousContracts = (await contracts).length
     const age = differenceInYears(customer.birthdate)
 
