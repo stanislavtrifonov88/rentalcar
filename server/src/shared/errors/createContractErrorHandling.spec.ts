59116df3780b6659ccae8fef5888eec1d24338d3
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-18 17:08:49 +0300

    createContractErrorHandling tests new logic

--- server/src/shared/errors/createContractErrorHandling.spec.ts
+++ server/src/shared/errors/createContractErrorHandling.spec.ts
@@ -8,48 +8,10 @@ describe('createContractErrorHandling', () => {
 
   beforeEach(async () => {
     mockInput = {
-      borrowerFirstName: 'test',
-      borrowerLastName: 'test',
-      borrowerAge: 18,
+      phone: '359888111444',
       startDate: timeStamp(),
       contractEndDate: timeStamp(1),
     };
-  });
-
-  it('should get throw an error with the correct message when first name length is below minumum', async () => {
-    // Arrange
-    mockInput.borrowerFirstName = 't';
-
-    // Act && Assert
-
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.firstNameMinLength);
-  });
-
-  it('should get throw an error with the correct message when first name length is above maximum', async () => {
-    // Arrange
-    mockInput.borrowerFirstName = 'tttttttttttttttttttttttttttttttttttttttttttttttt';
-
-    // Act & Assert
-
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.firstNamemaxLength);
-  });
-
-  it('should get throw an error with the correct message when last name length is below minumum', async () => {
-    // Arrange
-    mockInput.borrowerLastName = 't';
-
-    // Act && Assert
-
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.lastNameMinLength);
-  });
-
-  it('should get throw an error with the correct message when last name length is above maximum', async () => {
-    // Arrange
-    mockInput.borrowerLastName = 'tttttttttttttttttttttttttttttttttttttttttttttttt';
-
-    // Act & Assert
-
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.lastNamemaxLength);
   });
 
   it('should get throw an error with the correct message when start date is not valid', async () => {
@@ -58,9 +20,21 @@ describe('createContractErrorHandling', () => {
 
     // Act & Assert
 
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.validStartDate);
+    expect(() => createContractErrorHandling(mockInput)).toThrowError(
+      errorMessages.validStartDate,
+    );
   });
 
+  it('should get throw an error with the correct message when start date is not valid', async () => {
+    // Arrange
+    mockInput.startDate = 'test';
+
+    // Act & Assert
+
+    expect(() => createContractErrorHandling(mockInput)).toThrowError(
+      errorMessages.validStartDate,
+    );
+  });
 
   it('should get throw an error with the correct message when start date is not valid', async () => {
     // Arrange
@@ -68,7 +42,9 @@ describe('createContractErrorHandling', () => {
 
     // Act & Assert
 
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.validEndDate);
+    expect(() => createContractErrorHandling(mockInput)).toThrowError(
+      errorMessages.validEndDate,
+    );
   });
 
   it('should get throw an error with the correct message when start date is in the past', async () => {
@@ -77,7 +53,9 @@ describe('createContractErrorHandling', () => {
 
     // Act & Assert
 
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.startDateInThePast);
+    expect(() => createContractErrorHandling(mockInput)).toThrowError(
+      errorMessages.startDateInThePast,
+    );
   });
 
   it('should get throw an error with the correct message when return date is in the past', async () => {
@@ -87,15 +65,8 @@ describe('createContractErrorHandling', () => {
 
     // Act & Assert
 
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.returnDateInThePast);
-  });
-
-  it('should get throw an error with the correct message when age is below 18', async () => {
-    // Arrange
-    mockInput.borrowerAge = 17;
-
-    // Act & Assert
-
-    expect(() => createContractErrorHandling(mockInput)).toThrowError(errorMessages.ageBelow18);
+    expect(() => createContractErrorHandling(mockInput)).toThrowError(
+      errorMessages.returnDateInThePast,
+    );
   });
 });
