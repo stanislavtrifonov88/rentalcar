b280e7ae19ccaf143e5fed8122c7a1bc65cdcac9
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-19 17:40:17 +0300

    activeContractDTO created

---
+++ server/src/contracts/models/activeContractDTO.dto.ts
@@ -0,0 +1,29 @@
+export class ActiveContractDTO {
+  id: string;
+
+  startDate: string;
+
+  contractEndDate: string;
+
+  brand: string;
+
+  model: string;
+
+  phone: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  birthDate: string;
+
+  age: number;
+
+  price: number;
+
+  picture: string;
+
+  className: string;
+
+  previousContracts: number;
+}
