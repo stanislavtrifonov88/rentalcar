cab3590aeb99c912e844231c4faaf42cd64a15f0
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-20 09:17:19 +0300

    activeContract test updated

--- server/src/contracts/transformers/transformToActiveContractDTO.spec.ts
+++ server/src/contracts/transformers/transformToActiveContractDTO.spec.ts
@@ -6,44 +6,52 @@ describe('transformToReturnedCarDTO', () => {
     const mockContract: any = {
       test: 'test',
     };
-    const mockCar: any = {
-      test: 'test',
-    };
-
     const mockCustomer: any = {
       test: 'test',
     };
 
-    const transformToStandardContractDTO: any = jest.fn(() => {});
-    const transformToStandardCarDTO: any = jest.fn(() => {});
-    const transformToStandardCustomerDTO: any = jest.fn(() => {
+    const transformToStandardContractDTO: any = jest.fn(() => {
       return {
-        startDate: '2020-05-18T16:20:40.550Z',
-        contractEndDate: '2020-05-19T19:10:40.550Z',
+        id: 'b6f496c2-5d1c-404b-9496-bd6ee2b7f309',
+        startDate: '2020-05-20T05:44:00.000Z',
+        contractEndDate: '2020-05-26T22:01:00.000Z',
+        brand: 'Opel',
+        model: 'Astra',
         phone: '359888111444',
+        firstName: 'Asen',
+        lastName: 'Georgiev',
+        birthdate: '1968-04-29',
         age: 52,
         price: 70,
-        previousContracts: 55,
+      };
+    });
+    const transformToStandardCustomerDTO: any = jest.fn(() => {
+      return {
+        previousContracts: 27,
       };
     });
 
     const mockResult = {
-      startDate: '2020-05-18T16:20:40.550Z',
-      contractEndDate: '2020-05-19T19:10:40.550Z',
+      id: 'b6f496c2-5d1c-404b-9496-bd6ee2b7f309',
+      startDate: '2020-05-20T05:44:00.000Z',
+      contractEndDate: '2020-05-26T22:01:00.000Z',
+      brand: 'Opel',
+      model: 'Astra',
       phone: '359888111444',
+      firstName: 'Asen',
+      lastName: 'Georgiev',
+      birthdate: '1968-04-29',
       age: 52,
       price: 70,
-      previousContracts: 55,
+      previousContracts: 27,
     };
 
     // Act
 
     const result = await transformToActiveContractDTO(
       mockContract,
-      mockCar,
       mockCustomer,
       transformToStandardContractDTO,
-      transformToStandardCarDTO,
       transformToStandardCustomerDTO,
     );
 
