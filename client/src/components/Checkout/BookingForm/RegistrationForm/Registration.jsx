c260bb41da37261f655a07144b162f17f532b276
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-20 18:02:16 +0300

    moving statemanagement to Registration.jsx

--- client/src/components/Checkout/BookingForm/RegistrationForm/Registration.jsx
+++ client/src/components/Checkout/BookingForm/RegistrationForm/Registration.jsx
@@ -1,8 +1,86 @@
 import React from 'react';
-import { timeStamp } from '../../../../shared/dateModifiers';
 import { isValidField } from '../../Validations/validationChecks';
+import { observer, inject } from "mobx-react";
+import PropTypes from 'prop-types';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import { parsePhoneNumber } from "react-phone-number-input";
+import { checkInputValidity } from "../../Validations/validationChecks";
+import { fetchRequest, fetchRequestCustomer } from "../../../../services/Rest";
+import {
+  baseURL,
+  contracts,
+  cars,
+  customers,
+} from "../../../../services/restAPIs/restAPIs";
+import {
+  bookingFormErrors,
+  carCheckoutErrors,
+} from "../../../../services/toastify/toastifyHelpers";
+import { toastSuccess } from "../../../../services/toastify/toastify";
+import { timeStamp, differenceInYears } from "../../../../shared/dateModifiers";
 
-const Registration = ({ newCustomerHandler, registrationFormValidations, onCancel, onRegistrationSubmit }) => {
+// inject("colors")(observer(({ colors, label, onClick })
+// const Registration = inject("customerStore", "individualCarStore", "checkoutFormStore")(observer(({ 
+//   customerStore, individualCarStore, checkoutFormStore, newCustomerHandler, registrationFormValidations, onCancel, onRegistrationSubmit 
+// }) => {
+//   const validationErrorFirstName = isValidField(registrationFormValidations.firstName);
+//   const validationErrorLastName = isValidField(registrationFormValidations.lastName);
+//   const validationErrorBirthdate = isValidField(registrationFormValidations.birthdate);
+
+@inject("customerStore", "individualCarStore", "checkoutFormStore")
+@observer
+export default class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.customerStore = this.props.customerStore;
+    this.individualCarStore = this.props.individualCarStore;
+    this.checkoutFormStore = this.props.checkoutFormStore;
+  }
+
+  newCustomerHandler = (event) => {
+    const name = event.target.dataset.name;
+    const value = event.target.value;
+    const newObj = this.customerStore.newCustomer;
+    const { registrationFormValidations } = this.customerStore;
+    const validationObj = registrationFormValidations;
+    newObj[name] = value;
+    newObj.phone = this.customerStore.phone.value;
+    validationObj[name].valid = checkInputValidity(
+      newObj[name],
+      registrationFormValidations[name].rules
+    );
+    validationObj[name].touched = true;
+
+    this.customerStore.newCustomer = Object.assign(this.customerStore.newCustomer, newObj);
+    this.customerStore.registrationFormValidations = Object.assign(this.customerStore.registrationFormValidations, validationObj);
+  };
+
+  onRegistrationSubmit = (event) => {
+    const { newCustomer, registrationFormValidations } = this.customerStore;
+    const age = differenceInYears(newCustomer.birthdate);
+    bookingFormErrors(newCustomer, registrationFormValidations);
+    if (
+      !registrationFormValidations.firstName.valid ||
+      !registrationFormValidations.lastName.valid ||
+      !(age >= 18) ||
+      !registrationFormValidations.birthdate.touched ||
+      !registrationFormValidations.birthdate.valid
+    ) {
+      return;
+    }
+    fetchRequest(`${baseURL}/${customers}`, "POST", newCustomer).then(
+      (response) => {
+        this.customerStore.foundCustomer = response;
+      }
+    );
+  };
+
+  onCancel = (event) => this.props.history.push({ pathname: "/" });
+
+  render() {
+  const { foundCustomer, phone, newCustomer, registrationFormValidations } = this.customerStore;
+
   const validationErrorFirstName = isValidField(registrationFormValidations.firstName);
   const validationErrorLastName = isValidField(registrationFormValidations.lastName);
   const validationErrorBirthdate = isValidField(registrationFormValidations.birthdate);
@@ -22,7 +100,7 @@ const Registration = ({ newCustomerHandler, registrationFormValidations, onCance
             type="text"
             placeholder="First name"
             data-name="firstName"
-            onChange={newCustomerHandler}
+            onChange={this.newCustomerHandler}
           />
 
           <p className={validationErrorFirstName}>First name is invalid.</p>
@@ -38,7 +116,7 @@ const Registration = ({ newCustomerHandler, registrationFormValidations, onCance
             type="text"
             placeholder="Last name"
             data-name="lastName"
-            onChange={newCustomerHandler}
+            onChange={this.newCustomerHandler}
           />
 
           <p className={validationErrorLastName}>Last name is invalid.</p>
@@ -56,19 +134,19 @@ const Registration = ({ newCustomerHandler, registrationFormValidations, onCance
             placeholder="Birthdate"
             required
             data-name="birthdate"
-            onChange={newCustomerHandler}
+            onChange={this.newCustomerHandler}
           />
 
           <p className={validationErrorBirthdate}>Please enter a valid birthdate.</p>
         </div>
       </div>
       <div className="checkoutBtnsContainer">
-        <button type="submit" className="bookCarBtn" onClick={onRegistrationSubmit} data-element="bookingFormCheckoutBtn">
+        <button type="submit" className="bookCarBtn" onClick={this.onRegistrationSubmit} data-element="bookingFormCheckoutBtn">
           Register
         </button>
-        <button type="submit" className="bookCarBtn" onClick={onCancel}>Cancel</button>
+        <button type="submit" className="bookCarBtn" onClick={this.onCancel}>Cancel</button>
       </div>
     </>
   );
-};
-export default Registration;
+}};
+
