7a01af004a8b138b4f8485187473f8e0efe2ee6d
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-20 19:14:40 +0300

    Customer.jsx with mobX and state mng fns

--- client/src/components/Checkout/BookingForm/Customer/Customer.jsx
+++ client/src/components/Checkout/BookingForm/Customer/Customer.jsx
@@ -1,12 +1,88 @@
 import React from 'react';
-import { timeStamp } from '../../../../shared/dateModifiers';
 import { isValidField } from '../../Validations/validationChecks';
+import { observer, inject } from "mobx-react";
+import PropTypes from 'prop-types';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import { parsePhoneNumber } from "react-phone-number-input";
+import { checkInputValidity } from "../../Validations/validationChecks";
+import { fetchRequest, fetchRequestCustomer } from "../../../../services/Rest";
+import {
+  baseURL,
+  contracts,
+  cars,
+  customers,
+} from "../../../../services/restAPIs/restAPIs";
+import {
+  bookingFormErrors,
+  carCheckoutErrors,
+} from "../../../../services/toastify/toastifyHelpers";
+import { toastSuccess } from "../../../../services/toastify/toastify";
+import { timeStamp, differenceInYears } from "../../../../shared/dateModifiers";
 
+// const Customer = ({
+//   foundCustomer, onCheckoutInputSubmit, onCancel, checkoutFormValidations, carCheckoutHandler,
+// }) => {
 
-const Customer = ({
-  foundCustomer, onCheckoutInputSubmit, onCancel, checkoutFormValidations, carCheckoutHandler,
-}) => {
-  const validationErrorReturnDate = isValidField(checkoutFormValidations.contractEndDate);
+  @inject("customerStore", "individualCarStore", "checkoutFormStore")
+  @observer
+  export default class Customer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.customerStore = this.props.customerStore;
+      this.individualCarStore = this.props.individualCarStore;
+      this.checkoutFormStore = this.props.checkoutFormStore;
+    }
+
+    carCheckoutHandler = (event) => {
+      const name = event.target.dataset.name;
+      const value = event.target.value;
+      const newObj = {};
+      const validationObj = this.checkoutFormStore.checkoutFormValidations;
+      newObj[name] = value;
+      newObj.phone = this.customerStore.phone.value;
+      newObj.startDate = timeStamp();
+      validationObj[name].valid = checkInputValidity(
+        newObj[name],
+        this.checkoutFormStore.checkoutFormValidations[name].rules
+      );
+      validationObj[name].touched = true;
+  
+      this.checkoutFormStore.checkoutForm = newObj;
+      this.checkoutFormStore.checkoutFormValidations = validationObj;
+    };
+  
+    onCheckoutInputSubmit = (event) => {
+      const { checkoutForm, checkoutFormValidations } = this.checkoutFormStore;
+      event.preventDefault();
+      carCheckoutErrors(checkoutForm, checkoutFormValidations);
+      if (
+        !checkoutFormValidations.contractEndDate.touched ||
+        !checkoutFormValidations.contractEndDate.valid
+      ) {
+        return;
+      }
+      this.individualCarStore.loading = true;
+      fetchRequest(
+        `${baseURL}/${contracts}/car/${this.individualCarStore.car.id}`,
+        "POST",
+        checkoutForm
+      ).then((response) => {
+        this.props.onPageChangeToDashboard,
+        this.individualCarStore.loading = false,
+        toastSuccess("Car successfully borrowed")
+        // this.props.history.push({ pathname: "/dashboard" }),
+      });
+    };
+  
+    // onCancel = (event) => this.props.history.push({ pathname: "/" });
+
+  render() {
+    const { foundCustomer, phone, newCustomer, registrationFormValidations } = this.customerStore;
+    const { car, loading } = this.individualCarStore;
+    const { checkoutFormValidations } = this.checkoutFormStore;
+
+    const validationErrorReturnDate = isValidField(checkoutFormValidations.contractEndDate);
 
   return (
     <>
@@ -66,18 +142,18 @@ const Customer = ({
             required
             data-name="contractEndDate"
             min={timeStamp()}
-            onChange={carCheckoutHandler}
+            onChange={this.carCheckoutHandler}
           />
           <p className={validationErrorReturnDate}>Please enter a valid date.</p>
         </div>
       </div>
       <div className="checkoutBtnsContainer">
-        <button type="submit" className="bookCarBtn" onClick={onCheckoutInputSubmit} data-element="bookingFormCheckoutBtn">
+        <button type="submit" className="bookCarBtn" onClick={this.onCheckoutInputSubmit} data-element="bookingFormCheckoutBtn">
           Checkout
         </button>
-        <button type="submit" className="bookCarBtn" onClick={onCancel}>Cancel</button>
+        <button type="submit" className="bookCarBtn" onClick={this.props.onCancel}>Cancel</button>
       </div>
     </>
   );
-};
-export default Customer;
+}};
+
