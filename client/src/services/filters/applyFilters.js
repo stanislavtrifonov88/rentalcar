162eb4bfa2df221f2ceecec794b26c3ca83a003a
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-05-27 11:26:30 +0300

    applyFilters linting errors fixed

--- client/src/services/filters/applyFilters.js
+++ client/src/services/filters/applyFilters.js
@@ -1,10 +1,13 @@
 const applyFilters = (arrayToFilter, criteria) => {
   let filteredArray = arrayToFilter;
   if (criteria.className) {
-    filteredArray = filteredArray.filter((item) => item.className.toLowerCase().search(criteria.className.trim().toLowerCase()) !== -1);
+    filteredArray = filteredArray.filter((item) => (
+      item.className.toLowerCase().search(criteria.className.trim().toLowerCase()) !== -1));
   }
-  filteredArray = filteredArray.filter((item) => item.brand.toLowerCase().search(criteria.brand.trim().toLowerCase()) !== -1);
-  filteredArray = filteredArray.filter((item) => item.model.toLowerCase().search(criteria.model.trim().toLowerCase()) !== -1);
+  filteredArray = filteredArray.filter((item) => (
+    item.brand.toLowerCase().search(criteria.brand.trim().toLowerCase()) !== -1));
+  filteredArray = filteredArray.filter((item) => (
+    item.model.toLowerCase().search(criteria.model.trim().toLowerCase()) !== -1));
 
   return filteredArray;
 };
