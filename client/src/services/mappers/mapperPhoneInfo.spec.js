3fe01bc0de4c74125581c2f4b1cd4ee9c24c95be
stanislavtrifonov88 <stanislav.trifonov1@gmail.com>
2020-06-02 10:16:22 +0300

    phone library mapper tests

--- client/src/services/mappers/mapperPhoneInfo.spec.js
+++ client/src/services/mappers/mapperPhoneInfo.spec.js
@@ -1,1 +1,46 @@
 import mapperPhoneInfo from './mapperPhoneInfo';
+
+describe('mapperPhoneInfo', () => {
+
+  it('mapperPhoneInfo should return country and number from the phone library output', () => {
+      // Arramge
+      const inputMock = { 
+        countryCallingCode: "359",
+        nationalNumber: "888111444",
+        number: "+359888111444",
+        metadata: {},
+        country: "BG",
+      }
+      const resultMock = {
+        country: "BG",
+        number: "+359888111444",};
+      // Act
+  
+      const result = mapperPhoneInfo(inputMock);
+  
+      // Assert
+  
+      expect(result).toEqual(resultMock);
+    });
+
+    it('mapperPhoneInfo should return country and number from the phone library output', () => {
+        // Arramge
+        const inputMock = { 
+          countryCallingCode: "40",
+          nationalNumber: "888111444",
+          number: "+40888111444",
+          metadata: {},
+          country: "RO",
+        }
+        const resultMock = {
+          country: "RO",
+          number: "+40888111444",};
+        // Act
+    
+        const result = mapperPhoneInfo(inputMock);
+    
+        // Assert
+    
+        expect(result).toEqual(resultMock);
+      });
+});
